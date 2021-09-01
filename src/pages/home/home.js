f36ddd10943dd36da306bc3e435787f4ec444ef6
NazarSlupskyi <n.slupskyi@gmail.com>
2021-09-01 22:24:17 +0300

    Added main components

---
+++ src/pages/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './home.css';
+import Header from '../../components/header/header.js';
+import Footer from '../../components/footer/footer.js';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
